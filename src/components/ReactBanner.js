1c9b03f39995e59fa6ead933141b7ba4fc721c78
vikramsingh3 <s.vikram0110@gmail.com>
2022-03-30 19:53:06 +0530

    employee directory project init

--- src/components/ReactBanner.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ReactBanner = ({ name }) => {
-  return <h1>This is a react application with route {name}</h1>;
-};
-
-export default ReactBanner;
--- src/root.component.js
+++ src/root.component.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./root.component.css";
 import Home from "./pages/Home";
-import Page2 from "./pages/Page2";
 
 export default function Root(props) {
   return (
@@ -9,7 +8,6 @@ export default function Root(props) {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/reactPage2" element={<Page2 />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
